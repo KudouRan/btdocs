1197567c52391c8896e322153af4b1dc9d4da8ba
KudouRan <kudou.ran@outlook.com>
2022-06-18 01:45:51 +0800

    locales docsearchPlugin

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -25,6 +25,14 @@ export default defineUserConfig({
     editLinkText: '在 GitHub 上编辑此页',
     lastUpdatedText: '上次更新',
     contributorsText: '贡献者',
+    backToHome: '返回首页',
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    openInNewWindow: '在新窗口打开',
+    toggleSidebar: '切换侧边栏',
+    toggleColorMode: '切换主题',
+    notFound: ['抱歉，您要查找的页面不存在。'],
     navbar: [
       {
         text: '首页',
@@ -192,6 +200,49 @@ export default defineUserConfig({
       appId: '4HX8QKEWIN',
       apiKey: '753d7939116dbb6824909b0b0345864b',
       indexName: 'btvercel',
+      locales: {
+        '/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索...',
+              buttonAriaLabel: '搜索文档',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
+              },
+            },
+          },
+        },
+      },
     }),
     registerComponentsPlugin({
       componentsDir: vuepressPath.resolve(
