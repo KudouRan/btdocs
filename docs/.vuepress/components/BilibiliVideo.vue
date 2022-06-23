c13507bd20e122a6623857927fdc96cf5b61c9fd
KudouRan <kudou.ran@outlook.com>
2022-06-23 17:54:48 +0800

    update biliVideo

---
+++ docs/.vuepress/components/BilibiliVideo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <iframe
+      class="bilibili-video"
+      :src="url"
+      scrolling="no"
+      border="0"
+      frameborder="no"
+      framespacing="0"
+      allowfullscreen="true"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 获取 props 参数
+const props = defineProps<{
+  bv: string;
+  page?: string | number;
+}>();
+const url = `//player.bilibili.com/player.html?bvid=${props.bv}&page=${
+  props.page || 1
+}`;
+</script>
+
+<style scoped>
+.bilibili-video {
+  width: 100%;
+  height: 500px;
+  max-width: 100%;
+  align: center;
+  padding: 20px 0;
+}
+</style>
--- docs/guide/qinglong.md
+++ docs/guide/qinglong.md
@@ -6,7 +6,6 @@ description: 青龙面板运行
 
 ## 青龙面板文档 <TestedVersion type="ql" />
 
-- 文件名 `cat_bili_ql.js`
 - 默认配置文件名 `cat_bili_config.json`，或 `.json5`
 - 配置文件名也可以通过环境变量 `BILITOOLS_FILE_NAME` 指定，后缀 `.json` 可以省略
 - 由于作者并不熟悉，建议直接看青龙面板教程 <https://github.com/whyour/qinglong>
@@ -47,3 +46,7 @@ ql raw https://raw.githubusercontent.com/Kudouran/BiliTools/main/tools/bilitools
 或者使用环境变量
 
 ![环境变量](https://user-images.githubusercontent.com/81743795/148545529-73c95e3d-219f-4bf9-b266-7ad03bd9e9be.png)
+
+## 参考视频
+
+<BilibiliVideo bv="BV1fB4y1i7hL" />
