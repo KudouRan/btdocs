8d36b420f5870c6aaa9e206cd7f0c9e936cd044b
OceanJiang <catlair@qq.com>
2022-07-11 01:16:41 +0800

    Update cookie.ts

--- docs/.vuepress/utils/cookie.ts
+++ docs/.vuepress/utils/cookie.ts
@@ -1,26 +1,11 @@
-function getCookieArray(cookie: string | undefined) {
-  if (!cookie) return [];
-  return cookie.split('; ').map((el) => el.split('='));
-}
-
-function cookie2Obj(cookie: string): object {
-  const arr = getCookieArray(cookie).filter((el) => el.length === 2);
-  const obj = {};
-  for (const it of arr) {
-    if (obj[it[0]]) {
-      obj[it[0]] = it[1];
-    } else {
-      Object.defineProperty(obj, it[0], {
-        value: it[1],
-        enumerable: true,
-        writable: true,
-      });
-    }
-  }
-  Object.keys(obj).forEach((key) => {
-    obj[key] = encodeCookieValue(obj[key]);
-  });
-  return obj;
+function getCookieJSON(cookie: string | undefined): Record<string, string> {
+  if (!cookie) return {};
+  // 使用正则表达式获取 cookie 键值对，并转换为对象
+  return cookie.match(/([^;=]+)(?:=([^;]*))?/g).reduce((pre, cur) => {
+    const [key, value] = cur.trim().split('=');
+    pre[key] = value;
+    return pre;
+  }, {});
 }
 
 function encodeCookieValue(val: string): string {
@@ -32,7 +17,9 @@ function encodeCookieValue(val: string): string {
 }
 
 export function encodeCookie(cookie: string) {
-  return getCookieString(cookie2Obj(cookie));
+  const cookieJson = getCookieJSON(cookie);
+  Object.entries(cookieJson).forEach(([key, val]) => (cookieJson[key] = encodeCookieValue(val)));
+  return getCookieString(cookieJson);
 }
 
 function getCookieString(obj: object): string {
