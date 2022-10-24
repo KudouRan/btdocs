543295a05542f80737a7e422fac1551943b289fe
KudouRan <kudou.ran@outlook.com>
2022-10-24 10:32:30 +0800

    fix /api/releases

--- docs/.vuepress/stores/releases.ts
+++ docs/.vuepress/stores/releases.ts
@@ -6,13 +6,11 @@ export const useReleasesStore = defineStore('releases', () => {
   const tagName = ref('v0.5.7');
   const isRefreshed = ref(false);
 
-  useFetch<string>('https://btdocs.vercel.app/api/releases').then(
-    ({ data }) => {
-      tagName.value =
-        JSON.parse(data.value as any).data.tag_name || tagName.value;
-      isRefreshed.value = true;
-    }
-  );
+  useFetch<string>('/api/releases').then(({ data }) => {
+    tagName.value =
+      JSON.parse(data.value as any).data.tag_name || tagName.value;
+    isRefreshed.value = true;
+  });
 
   return { tagName, isRefreshed };
 });
