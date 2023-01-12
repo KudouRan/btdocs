1eb7c18074e0d4c909e15456c3a2c29f409fabe8
KudouRan <kudou.ran@outlook.com>
2023-01-12 21:58:35 +0800

    up

--- api/version.ts
+++ api/version.ts
@@ -2,8 +2,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   return response.status(200).json({
-    data: {
-      tag_name: 'v0.22.1209',
-    },
+    tag_name: 'v0.22.1209',
   });
 };
