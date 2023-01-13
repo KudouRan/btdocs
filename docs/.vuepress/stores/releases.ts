c5fb229c7f975c010aee1bfa2799ac7fba2c57b3
KudouRan <kudou.ran@outlook.com>
2023-01-13 11:41:26 +0800

    up

--- docs/.vuepress/stores/releases.ts
+++ docs/.vuepress/stores/releases.ts
@@ -1,16 +1,16 @@
 import { defineStore } from 'pinia';
-import { useFetch } from '@vueuse/core';
+// import { useFetch } from '@vueuse/core';
 import { ref } from 'vue';
 
 export const useReleasesStore = defineStore('releases', () => {
-  const tagName = ref('v0.6.221018');
-  const isRefreshed = ref(false);
+  const tagName = ref(import.meta.env.VITE_TAG_NAME);
+  const isRefreshed = ref(true);
 
-  useFetch<string>('/api/version').then(({ data }) => {
-    tagName.value =
-      JSON.parse(data.value as any).data.tag_name || tagName.value;
-    isRefreshed.value = true;
-  });
+  // useFetch<string>('/api/version').then(({ data }) => {
+  //   tagName.value =
+  //     JSON.parse(data.value as any).data.tag_name || tagName.value;
+  //   isRefreshed.value = true;
+  // });
 
   return { tagName, isRefreshed };
 });
