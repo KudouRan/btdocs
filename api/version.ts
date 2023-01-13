6d1b50c73da796b3bade8ae661d6335bdc8c69a1
KudouRan <kudou.ran@outlook.com>
2023-01-13 11:55:06 +0800

    up

--- api/version.ts
+++ api/version.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   return response.status(200).json({
-    tag_name: import.meta.env.VITE_TAG_NAME,
+    tag_name: process.env.VERVEL_TAG_NAME,
     notices: ['本项目开源且免费，文档地址 https://b.2024666.xyz'],
   });
 };
